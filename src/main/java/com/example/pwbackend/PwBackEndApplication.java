7540a5c04055bc0394372f266774a9f6cc211812
Rugescu Razvan <razvan.rugescu02@e-uvt.ro>
2023-03-27 02:05:54 +0300

    Spring boot with swagger

---
+++ src/main/java/com/example/pwbackend/PwBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.pwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pwbackend/PwBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pwbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
